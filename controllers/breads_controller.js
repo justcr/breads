64bae5e5f40de5af972691b67e2fc75e518af916
Carlos Rodriguez <crrodriguez.0430@gmail.com>
2022-01-15 10:53:51 -0800

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -16,8 +16,14 @@ module.exports = breads
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-  })
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.render('404')
+  }
+})
 
   // INDEX
 breads.get('/', (req, res) => {
